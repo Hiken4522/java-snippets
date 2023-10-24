d1de937e9bd4c22780bf5bcbc62a1a676f831937
Hiken4522 <113972660+Hiken4522@users.noreply.github.com>
2023-10-24 22:21:09 +0530

    Create usefulstringmethods.java

---
+++ usefulstringmethods.java
@@ -0,0 +1,20 @@
+
+
+public class App{
+    public static void main(String[] args){
+
+        String name = "Bro";
+        boolean result = name.equals("bro");//to compare two strings
+        boolean res = name.equalsIgnoreCase("bro");//to compare two strings by ingnoring case
+        int resu = name.length();//to find the length of the string
+        char resul = name.charAt(0);//to find the char at a particular index
+        int re = name.indexOf("B");//to find the index of a particular character
+        boolean r = name.isEmpty();//to check of the string is empty or not
+        String re1 = name.toUpperCase();//it will change all characters to uppercase
+        String re2 = name.toLowerCase();//it will change all characters to lowercase
+        String re4 = name.trim();//it will remove all of the empty spaces before and after the characters
+        String re5 = name.replace('o','a');
+       
+
+    }
+}
